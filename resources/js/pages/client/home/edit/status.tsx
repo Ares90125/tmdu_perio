babeb2e1bf883c6cd668560bfce83879e0fd8c72
Ares <queeNbee90125@gmail.com>
2022-05-16 18:17:06 +0900

    commit

--- resources/js/pages/client/home/edit/status.tsx
+++ resources/js/pages/client/home/edit/status.tsx
@@ -42,8 +42,7 @@ const Status = () => {
             <TypeHeader text="朝のお口の状態"/>
             <div className="px-8 pt-4">
                 <p className="text-sm text-mainColor text-left pb-2">今朝のお口の状態はいかがでしたか？</p>
-                <select id="fruits" value={selectindex!} className="flex items-center justify-center bg-white text-mainColor  my-2 text-[26px] font-bold w-full rounded-lg border border-mainColor    outline-0 text-center" style={{appearance: "none"}}
-                    onChange={(e) => setSelect(e.target.value)}>
+                <select style={{WebkitAppearance: "none", appearance: "none",textAlignLast: "center",WebkitAlignContent:"center", textAlign:"center"}} value={selectindex!} className="flex items-center justify-center bg-white text-mainColor  my-2 text-[26px] font-bold w-full rounded-lg border border-mainColor    outline-0 text-center"
                     <option value={1}>すっきりしている</option>
                     <option value={2}>特に問題なし</option>
                     <option value={3}>軽い違和感</option>
