e19405a20b2baf264cce738c927f305542983b36
Ares <queeNbee90125@gmail.com>
2022-07-08 09:05:31 -0700

    add comit

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'id',
+        'LineId',
         'userid',
         'password',
     ];
